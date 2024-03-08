2f04c71a688e61f84c228f9766fe1db730994e6b
StuvidLabs <yanwarsolah@gmail.com>
2024-03-08 19:59:39 +0700

    1

--- pertemuan10/script.js
+++ pertemuan10/script.js
@@ -1,20 +1,37 @@
-class Car {
-  constructor(name) {
-    this.name = name;
-  }
+// const hero = {
+//   title: "Vexana",
+//   type: "Mage",
+//   damage: 60,
+// }
 
-  tampil() {
-    return this.name
-  }
+// const {damager = 10, title, type, } = hero;
+
+// console.log(`title: ${title}`);
+// console.log(`type: ${type}`);
+// console.log(`damage: ${damager}`);
+
+const penjualan = {
+  faktur: "FKT001",
+  customer: {
+    name: "Andika",
+    phone: "0898162726256"
+  },
 }
 
-class Sport extends Car {
-  tujuan() {
-    return `Mobil ${this.name} untuk pribadi`
-  }
+const posting = {
+  judul: "Lorem ipsum",
+  konten: "lorem ipsum konten blog post",
+  tags: ["#coding", "#js", "#FE"]
 }
 
-const bmw = new Sport("BMW");
-document.querySelector("h1").innerText = bmw.tampil()
-console.log(bmw.tampil())
-console.log(bmw.tujuan())
+const {faktur, customer: {name, phone}} = penjualan;
+// const { name, phone } = customer;
+console.log(`Faktur ${faktur}`);
+// console.log(`Customer ${name}`)
+console.log(`Customer Name ${name}`)
+console.log(`Customer Phone ${phone}`)
+
+const {tags: [tag1, tag2]} = posting
+
+console.log(tag1)
+console.log(tag2)
